6e1c364c5e739f2e763f2862f19697f75a9f43cf
Saif <saifobeidat@gmail.com>
2022-05-28 21:36:16 +0300

    add router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
+  router: {
+    base: "/yemeni-honey/",
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
