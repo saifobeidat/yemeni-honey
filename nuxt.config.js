95553d2d05173338ddf111932cf4cb0a75a310dd
Saif <saifobeidat@gmail.com>
2022-05-28 21:53:38 +0300

    add public path

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    publicPath: "https://saifobeidat.github.io/yemeni-honey",
     postcss: {
       plugins: {
         tailwindcss: {},
